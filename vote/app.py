4a5d60d730e9da63681009d9d1126989b3ca1a0f
subu123321 <emailssubu87@gmail.com>
2019-01-23 11:48:19 +0400

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -5,9 +5,9 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
-redis_password = os.getenv('REDIS_PASSWORD', "Dogs")
+option_a = os.getenv('OPTION_A', "BJP")
+option_b = os.getenv('OPTION_B', "CONGRESS")
+redis_password = os.getenv('REDIS_PASSWORD', "CONGRESS")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
